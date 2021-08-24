69e3ec5dd8ec73dda4b887478a32279e2134630e
hadi <hadi_960@hotmail.com>
2021-08-23 21:30:58 -0500

    fixes seed and styling

--- client/src/MovieList.js
+++ client/src/MovieList.js
@@ -233,6 +233,66 @@ function MovieList() {
             return movie.movie_type === 'comedy' && <SlimMovie key={movie.id} movie={movie} />
           })}
         </Carousel>
+        <br />
+        <br />
+
+        <h2>Super Hero</h2>
+        <Carousel
+          itemClass="carousel-item-padding-40-px"
+          additionalTransfrom={0}
+          arrows
+          autoPlaySpeed={3000}
+          centerMode={false}
+          className="carousel"
+          containerClass="container-with-dots"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside={true}
+          renderDotsOutside={false}
+          responsive={{
+          desktop: {
+          breakpoint: {
+          max: 3000,
+          min: 1024
+          },
+          items: 3,
+          partialVisibilityGutter: 40
+          },
+          mobile: {
+          breakpoint: {
+          max: 464,
+          min: 0
+          },
+          items: 1,
+          partialVisibilityGutter: 30
+          },
+          tablet: {
+          breakpoint: {
+          max: 1024,
+          min: 464
+          },
+          items: 2,
+          partialVisibilityGutter: 30
+          }
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          {movies && movies.map((movie) => {
+            return movie.movie_type === 'superhero' && <SlimMovie key={movie.id} movie={movie} />
+          })}
+        </Carousel>
+
+
+
+
       </div>
     )
 }

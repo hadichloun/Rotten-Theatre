95d2e8be4ba53f0f99456e01f67246d06bcac6f9
hadi chloun <hadi_960@hotmail.com>
2022-08-06 15:58:08 -0500

    Added fallback controller for react client side code

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   # signup route
   post "/signup" => "auth#signup"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
